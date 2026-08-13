c754b03812f552b8543a7d81801adc67df605016
Andreykaproger <korovalolka123456789@gmail.com>
2026-08-13 16:22:44 +0300

    fix: register EventSub router

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .eventsub import eventsub_router
 from .health import health_router
 from .streamers import streamers_router
 
@@ -7,3 +8,4 @@ api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(health_router, tags=["Health"])
 api_router.include_router(streamers_router, tags=["Streamers"])
+api_router.include_router(eventsub_router, tags=["EventSub"])
